99f13d4869c77696dc527e60246d9111487d9744
michael <mich0330jon@gmail.com>
2021-02-04 16:54:32 -0300

    implementação do menu e botão toggle

--- app/src/main/java/com/example/testfragment/MainActivity.kt
+++ app/src/main/java/com/example/testfragment/MainActivity.kt
@@ -2,9 +2,15 @@ package com.example.testfragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.view.MenuItem
 import android.view.View
 import android.widget.Button
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
 import com.example.testfragment.fragments.ConsolesFragment
 import com.example.testfragment.fragments.GamesFragment
 import com.example.testfragment.fragments.HomeFragment
@@ -27,6 +33,10 @@ class MainActivity : AppCompatActivity(),
     private lateinit var bottomNavigation: BottomNavigationView
 
     private lateinit var navigationView: NavigationView
+
+    private lateinit var toolbar: Toolbar
+
+    private lateinit var drawer: DrawerLayout
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,6 +63,22 @@ class MainActivity : AppCompatActivity(),
 
         navigationView = findViewById(R.id.navigation_view)
         navigationView.setNavigationItemSelectedListener(this)
+
+        toolbar = findViewById(R.id.toolbar)
+        toolbar.setTitle("Home")
+        setSupportActionBar(toolbar)
+
+
+        drawer = findViewById(R.id.drawer)
+
+        // **** Adicionar o botão que abre o drawer menu
+        var toggle = ActionBarDrawerToggle(this, drawer, toolbar,R.string.open_drawer, R.string.close_drawer)
+
+        drawer.addDrawerListener(toggle)
+        toggle.syncState()
+
+      setFragment(homeFragment)
+
     }
 
     override  fun onClick(v: View){
@@ -75,17 +101,34 @@ class MainActivity : AppCompatActivity(),
 
         when(item.itemId) {
             R.id.menu_home -> {
-                supportFragmentManager.beginTransaction().replace(R.id.frame, homeFragment).commit()
+                toolbar.setTitle("Home")
+               setFragment(homeFragment)
             }
 
             R.id.menu_console -> {
-                supportFragmentManager.beginTransaction().replace(R.id.frame, consoleFragment).commit()
+                toolbar.setTitle("Consoles")
+                setFragment(consoleFragment)
             }
 
             R.id.menu_game -> {
-                supportFragmentManager.beginTransaction().replace(R.id.frame, gamesFragment).commit()
+                toolbar.setTitle("Games")
+                setFragment(gamesFragment)
             }
         }
+
+        //*** Selecionar o item de menu correto na BottomNavigationView
+        var selectedMenu = bottomNavigation.menu.findItem(item.itemId)
+        selectedMenu.setChecked(true)
+
+        // *** Fechar o Drawer se estiver aberto
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START)
+        }
+
         return true
+    }
+
+    fun setFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().replace(R.id.frame, fragment).commit()
     }
 }
--- app/src/main/java/com/example/testfragment/fragments/GamesFragment.kt
+++ app/src/main/java/com/example/testfragment/fragments/GamesFragment.kt
@@ -1,10 +1,8 @@
 package com.example.testfragment.fragments
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.testfragment.R
@@ -46,9 +44,15 @@ class GamesFragment : Fragment() {
         // Atualizar a lista de jogos do adapter
         gameAdapter.updateGameList(gameList)
 
-
+        // tornar o menu visivel
+        setHasOptionsMenu(true)
 
         return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_game, menu)
     }
 
 }
